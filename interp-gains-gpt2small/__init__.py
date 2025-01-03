545f52ec790a2ba213b4a683ea93b2b7ae489a00
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2025-01-03 16:16:31 +0530

    accumulating all activations of mlp in

--- interp-gains-gpt2small/__init__.py
+++ interp-gains-gpt2small/__init__.py
@@ -1,8 +1,15 @@
 import nnsight
 from nnsight import LanguageModel
-from transformer_lens import HookedTransformer
+# from transformer_lens import HookedTransformer
 import torch
 import transformers
 import json
 from tqdm import tqdm
 import yaml
+
+
+from transformer_lens.evals import make_wiki_data_loader
+from transformer_lens.HookedTransformer import HookedTransformer
+import logging
+import os
+import pickle as pkl
