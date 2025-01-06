e06a30c8b88dbe24522035aef68ccff133e21c52
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2025-01-06 06:08:52 +0530

    updating scripts

--- interp-gains-gpt2small/scripts/setup.sh
+++ interp-gains-gpt2small/scripts/setup.sh
@@ -6,5 +6,5 @@ gdown https://drive.google.com/uc?id=1TkWHR-2EGEXxHw4sRN6V6CdN-G2O8RPG
 mv wiki_fully_modular_mlp_in_out.pt interp-gains-gpt2small/data/wiki_fully_modular_mlp_in_out.pt
 
 gdown https://drive.google.com/uc?id=1J6way5S9XUVTUxIe6o2fdFQQCr1kMaSv
-mv wiki_non_modular_mlp_in_out.pt interp-gains-gpt2small/data/wiki_fully_modular_mlp_in_out.pt
+mv wiki_non_modular_mlp_in_out.pt interp-gains-gpt2small/data/wiki_non_modular_mlp_in_out.pt
 
