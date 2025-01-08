bffdae95c354e8b36a25032af4a9c986dfcd660e
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2025-01-08 07:24:55 +0530

    analysing cluster correlations via activations

--- interp-gains-gpt2small/__init__.py
+++ interp-gains-gpt2small/__init__.py
@@ -3,9 +3,12 @@ from nnsight import LanguageModel
 # from transformer_lens import HookedTransformer
 import torch
 import transformers
+from transformers import GPT2Model, GPT2Tokenizer
 import json
 from tqdm import tqdm
 import yaml
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 
 from transformer_lens.evals import make_wiki_data_loader
--- interp-gains-gpt2small/activation.py
+++ interp-gains-gpt2small/activation.py
@@ -3,6 +3,10 @@ from __init__ import *
 
 def get_activations(args):
     
+    model =  GPT2Model.from_pretrained('gpt2')
+    
+    print(model)
+    
     model_nmod = HookedTransformer.from_pretrained('gpt2-small')
     model_mod = HookedTransformer.from_pretrained('gpt2-small')
     tokenizer = transformers.GPT2Tokenizer.from_pretrained('gpt2')
@@ -62,7 +66,57 @@ def get_activations(args):
     logging.info("Done\n\n\n")
     
     
+    
+def activation_analysis(args):
+    
 
+    def covariance_matrix(batch_):
+        values = torch.norm(batch_, dim = -1)
+        mean = values.mean(dim = 0)
+        centered = values - mean
+        
+        assert centered.size(1) == 1024
+        cluster1 = torch.mean(centered[:, :centered.size(1)//4], dim = 1).reshape(-1, 1)
+        cluster2 = torch.mean(centered[:, centered.size(1)//4:centered.size(1)//2], dim = 1).reshape(-1, 1)
+        cluster3 = torch.mean(centered[:, centered.size(1)//2:centered.size(1)*3//4], dim = 1).reshape(-1, 1)
+        cluster4 = torch.mean(centered[:, centered.size(1)*3//4:], dim = 1).reshape(-1, 1)
+        
+        all_cluster = torch.cat([cluster1, cluster2, cluster3, cluster4], dim = 1)
+        assert all_cluster.size() == (centered.size(0), 4)
+        
+        cov = torch.matmul(centered.T, centered) / (centered.size(0) - 1)
+        mean_cov = torch.matmul(all_cluster.T, all_cluster) / (centered.size(0) - 1)
+        return cov, mean_cov
+    
+    
+    def visualize(batch_):
+        cov, mean_cov = covariance_matrix(batch_)
+        # plt.figure(figsize=(10,10))
+        # sns.heatmap(cov, cmap='hot', annot=True, fmt='g')
+        # plt.show()    
+        # plt.close()
+        plt.figure(figsize=(10,10))
+        sns.heatmap(mean_cov, cmap='hot', annot=True, fmt='g')
+        plt.show()
+        plt.close()
+        
+    for files in os.listdir('interp-gains-gpt2small/data'):
+        if 'chunk0' in files:
+            batch = torch.load(f'interp-gains-gpt2small/data/{files}')
+            print(files)
+            break
+    
+    for layer_idx, values in batch.items():
+        print(layer_idx, values.size())
+        break
+    
+    # visualize(batch[6])
+    # _ = covariance_matrix(batch[6])
+
+
+
+
+    
 def main():
     
     parser = ArgumentParser()
@@ -71,7 +125,8 @@ def main():
 
     args = parser.parse_args()
     
-    get_activations(args)
+    # get_activations(args)
+    activation_analysis(args)
     
     
 
