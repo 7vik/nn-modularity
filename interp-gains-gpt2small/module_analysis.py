b599a01662f27ec93e51aca4d08f38e51e40a269
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2025-01-15 11:01:51 +0530

    data preparation for layer 6

--- interp-gains-gpt2small/module_analysis.py
+++ interp-gains-gpt2small/module_analysis.py
@@ -24,6 +24,9 @@ def config_(args):
     return model_nmod, model_mod, tokenizer, device
 
 
+def nps(arr_):
+    pprint(np.array(arr_).shape)
+
 
 def intervention(args, index):
     '''
@@ -35,8 +38,12 @@ def intervention(args, index):
     1. Switch off 1 module M, i.e. keep 3 modules on - for one layer we can also use this, but could be minimal.
     2. Switch off 3 modules, and keep 1 module M on - if the acc is reasonable we can have this only as internvention.
 
-    Do this for one layer 
+    Do type 1 intervention for one layer, i.e. Layer 6.
     For many layers we can focus on type 2 intervention.
+    
+    #TODO: Make graphs per data point also. if it does not happen then we can have a single graph showcasign the overlapping data for which the output was wrong.
+    #TODO: Make bar graph of accuracy for each type of intervention on each module.
+    #TODO: Repeat the above process for type 2 intervention.
     '''
     
     model_nmod, model_mod, tokenizer, device = config_(args)
@@ -52,6 +59,8 @@ def intervention(args, index):
 
     hook_ = model_mod.blocks[args.num_layer].mlp.hook_pre.register_forward_hook(hook_fn)
     
+    
+    
     def analysis():
         with open("interp-gains-gpt2small/data/cropped_dataset_topk1000_acc.pkl", "rb") as f:
             samples = pkl.load(f)   
@@ -60,17 +69,29 @@ def intervention(args, index):
         for sample_idx in tqdm(range(len(samples))):
             sample = samples[sample_idx].to(device)
             logits = model_mod(sample)
-            if int(sample[:,-1].item()) in [int(x) for x in logits[:,-1,:].topk(1000, dim = -1).indices.tolist()[0]]:
-                correct+=1
+            # if int(sample[:,-1].item()) in [int(x) for x in logits[:,-1,:].topk(1000, dim = -1).indices.tolist()[0]]:
+            #     correct += 1
+            # total+=1
+
+            # print(tokenizer.decode(sample[0].tolist()))
+            # print(f"\n\n\n\n")
+            predicted_string = []
+            # Decode each sequence individually
+            # for seq in logits.argmax(dim=-1).tolist():
+            #     predicted_string.append(tokenizer.decode(seq))
+            # print(" ".join(predicted_string))
+            # equals = (sample[:,1:].to("cpu") == logits[:,1:-1,:].argmax(dim = -1).to("cpu")).float().mean()  # -> method of Satvik.
+            if sample[:,-1].item() == logits[:,-2,:].argmax(dim = -1).item(): # -> my improved method.
+                correct += 1
             total+=1
-            
-            if sample_idx%100 == 0:
-                print(f"Accuracy: {correct/total}")
-        
+            if sample_idx%100 == 0 and sample_idx != 0:
+                print(f"Accuracy: {correct/total}")    
         return correct/total
     
     
-    def dataset_prepartion():
+    
+    
+    def dataset_prepartion(args):
         correct = 0; total = 0
         samples = []
         for idx, data_ in enumerate(tqdm(make_wiki_data_loader(tokenizer, batch_size=args.batch_size), 
@@ -82,7 +103,7 @@ def intervention(args, index):
             # print([int(x) for x in logits[:,-1,:].topk(3, dim = -1).indices.tolist()[0]])
             # print(int(data[:,-1].item()))
             # if data[:,-1].item() == logits.argmax(dim = -1).item():
-            if int(data[:,-1].item()) in [int(x) for x in logits[:,-1,:].topk(1000, dim = -1).indices.tolist()[0]]:
+            if data[:,-1].item() == logits[:,-2,:].argmax(dim = -1).item():
                 correct+=1
                 samples.append(data)
             total+=1
@@ -97,6 +118,7 @@ def intervention(args, index):
             Top 3: 2.4% 
             Top 100: 12%
             Top 1000: 54%
+            
             '''
             # all_loss.append(loss.item())
             if idx%100 == 0:
@@ -105,14 +127,14 @@ def intervention(args, index):
         hook_.remove()
         
         os.makedirs("interp-gains-gpt2small/data", exist_ok=True)
-        with open("interp-gains-gpt2small/data/cropped_dataset_topk1000_acc.pkl", "wb") as f:
+        with open(f"interp-gains-gpt2small/data/cropped_dataset_last_token_layer{args.num_layer}.pkl", "wb") as f:
             pkl.dump(samples, f)
         
         return correct/total
 
-    analysis()
-    # accuracy = dataset_prepartion()
-    # print(f"The accuracy of the trained model is {accuracy}")
+    # analysis()
+    accuracy = dataset_prepartion(args)
+    print(f"The accuracy of the trained model is {accuracy}")
     
 
 def visualize(dictionary):
@@ -134,7 +156,7 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('--device', type=str, default='cuda')
     parser.add_argument('--batch_size', type=int, default=1)
-    parser.add_argument('--num_layer', type=int, default=3)
+    parser.add_argument('--num_layer', type=int, default=6)
     
     args = parser.parse_args()
     
@@ -146,21 +168,24 @@ def main():
     layer_wise_loss_dict = {}
     all_sample_loss = {}
     
-    # all_sample_loss["baseline"] = intervention(args, "baseline")
+    all_sample_loss["baseline"] = intervention(args, "baseline")
     '''
     We should only process the dataset for which the trained model produces accurate output.
     '''
     
-    for i in tqdm(range(4)):
-        if i == 0:
-            # intervention(args, index1)
-            pass
-        # elif i == 1:
-        #     intervention(args, index2)
-        # elif i == 2:
-        #     intervention(args, index3)
-        elif i == 3:
-            intervention(args, index4)
+    # for i in tqdm(range(4)):
+    #     if i == 0:
+            # print(f"Intervention using the index {i} on layer {args.num_layer}")
+    #         intervention(args, index1)
+    #     elif i == 1:
+            # print(f"Intervention using the index {i} on layer {args.num_layer}")
+    #         intervention(args, index2)
+    #     elif i == 2:
+            # print(f"Intervention using the index {i} on layer {args.num_layer}")
+    #         intervention(args, index3)
+    #     elif i == 3:
+            # print(f"Intervention using the index {i} on layer {args.num_layer}")
+    #         intervention(args, index4)
     
     # Focusing Layers: 2,5,6,7,10
     # for layer_idx in tqdm(range(12)):
