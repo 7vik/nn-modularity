6aa7c9294a26abe99977091538bcb620e841d3d2
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2025-01-21 12:51:06 +0530

    non-modular model results

--- interp-gains-gpt2small/scripts/run.sh
+++ interp-gains-gpt2small/scripts/run.sh
@@ -4,8 +4,8 @@ python interp-gains-gpt2small/module_analysis.py --device "mps" --num_layer 2 --
 echo "Running module analysis for GPT-2 small for layer 5"
 python interp-gains-gpt2small/module_analysis.py --device "mps" --num_layer 5 --type_of_intervention "type1"
 
-# echo "Running module analysis for GPT-2 small for layer 6"
-# python interp-gains-gpt2small/module_analysis.py --device "mps" --num_layer 6 --type_of_intervention "type1"
+echo "Running module analysis for GPT-2 small for layer 6"
+python interp-gains-gpt2small/module_analysis.py --device "mps" --num_layer 6 --type_of_intervention "type1"
 
 echo "Running module analysis for GPT-2 small for layer 7"
 python interp-gains-gpt2small/module_analysis.py --device "mps" --num_layer 7 --type_of_intervention "type1"
